f7c5216547738b3af489ce511187baec65b96e8c
Kayla <kayla.ross@kw.com>
2020-01-16 12:27:49 -0700

    Commit 1

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,45 +1,46 @@
+
 {
 
 $('button#play-pause').on('click', function() {
-  player.playPause();
-     $(this).attr('playState', player.playState);
+player.playPause();
+   $(this).attr('playState', player.playState);
 });
-  $('button#next').on('click', function() {
-    if (player.playState !== 'playing') { return; }
+$('button#next').on('click', function() {
+  if (player.playState !== 'playing') { return; }
+
+  const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+  const nextSongIndex = currentSongIndex + 1;
+  if (nextSongIndex >= album.songs.length) { return; }
+  const nextSong = album.songs[nextSongIndex];
+  player.playPause(nextSong);
+});
+
+  $('button#previous').click(function() {
+    if(player.playState !== 'playing'){ return;}
 
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const nextSongIndex = currentSongIndex + 1;
-    if (nextSongIndex >= album.songs.length) { return; }
-    const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+    const previousSongIndex = currentSongIndex - 1;
+    if(previousSongIndex < 0) { return; }
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
   });
 
-    $('button#previous').click(function() {
-      if(player.playState !== 'playing'){ return;}
+  $('#time-control input').on('input', function (event) {
+     player.skipTo(event.target.value);
+ });
 
-      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-      const previousSongIndex = currentSongIndex - 1;
-      if(previousSongIndex < 0) { return; }
-      const previousSong = album.songs[previousSongIndex];
-      player.playPause(previousSong);
-    });
+  $('#volume-control input').on('input', function(event){
+   player.setVolume(event.target.value);
+ });
 
-    $('#time-control input').on('input', function (event) {
-       player.skipTo(event.target.value);
-   });
+  setInterval( () => {
+    if (player.playState !== 'playing') { return; }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( currentTime );
+    $('#time-control input').val(percent);
+    $('#time-control .total-time').text( duration );
+  }, 1000);
 
-    $('#volume-control input').on('input', function(event){
-     player.setVolume(event.target.value);
-   });
-
-    setInterval( () => {
-      if (player.playState !== 'playing') { return; }
-      const currentTime = player.getTime();
-      const duration = player.getDuration();
-      const percent = (currentTime / duration) * 100;
-      $('#time-control .current-time').text( currentTime );
-      $('#time-control input').val(percent);
-      $('#time-control .total-time').text( duration );
-    }, 1000);
-
-  }
+}
